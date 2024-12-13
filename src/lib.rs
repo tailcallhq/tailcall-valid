301bc1a047336833768a80de33072ba344404d97
Tushar Mathur <tusharmath@gmail.com>
2024-12-13 15:02:29 -0800

    feat: rename traits to ValidFrom and ValidInto

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,24 @@ pub use cause::*;
 pub use valid::*;
 
 /// Moral equivalent of TryFrom for validation purposes
-pub trait ValidateFrom<T>: Sized {
+pub trait ValidFrom<T>: Sized {
     type Error;
     type Trace;
-    fn validate_from(a: T) -> Valid<Self, Self::Error, Self::Trace>;
+    fn valid_from(a: T) -> Valid<Self, Self::Error, Self::Trace>;
 }
 
 /// Moral equivalent of TryInto for validation purposes
-pub trait ValidateInto<T> {
+pub trait ValidInto<T> {
     type Error;
     type Trace;
-    fn validate_into(self) -> Valid<T, Self::Error, Self::Trace>;
+    fn valid_into(self) -> Valid<T, Self::Error, Self::Trace>;
 }
 
 /// A blanket implementation for ValidateInto
-impl<S, T: ValidateFrom<S>> ValidateInto<T> for S {
+impl<S, T: ValidFrom<S>> ValidInto<T> for S {
     type Error = T::Error;
     type Trace = T::Trace;
-    fn validate_into(self) -> Valid<T, Self::Error, Self::Trace> {
-        T::validate_from(self)
+    fn valid_into(self) -> Valid<T, Self::Error, Self::Trace> {
+        T::valid_from(self)
     }
 }
